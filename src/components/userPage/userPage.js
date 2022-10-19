13bb951ba4de62e536f2d13c300692a81c32838a
ShaliniR8 <1705820@kiit.ac.in>
2022-10-19 03:36:55 -0500

    styling user page and card buttons to be responsive to media

--- src/components/userPage/userPage.js
+++ src/components/userPage/userPage.js
@@ -4,6 +4,8 @@ import { useDbData, useDbUpdate } from "../../utilities/firebase";
 import EventCard from "../eventCard/eventCard";
 import ConfirmModal from "../home-page/confirm";
 import { Container } from "react-bootstrap";
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 
 const UserPage = () => {
@@ -13,14 +15,9 @@ const UserPage = () => {
     const [evToDel, setEvToDel] = useState(-1);
     const [updateEvent, result] = useDbUpdate("/");
 
-
-
-    // data
     if (error) return <h1>Error loading data: {`${error}`}</h1>;
     if (data === undefined) return <h1>Loading data...</h1>;
     if (!data) return <h1>No data found</h1>;
-
-    // user
     if (profileError) return <h1>Error loading profile: {`${profileError}`}</h1>;
     if (profileLoading) return <h1>Loading user profile</h1>;
     if (!profile) return <h1>No profile data</h1>;  
@@ -29,49 +26,72 @@ const UserPage = () => {
     const currentUser = profile.user 
 
     const toggleEvent = (event, eventId, isCurrentUserOrganizer) => {
-    if (isCurrentUserOrganizer) {
-      setEvToDel(eventId);
-      setIsConfirmModalVisible(true);
-    } else {
-      if (event.attendees.includes(currentUser.uid)) {
-        event.size -= 1;
-        event.attendees = event.attendees.filter((e) => e !== currentUser.uid);
+      if (isCurrentUserOrganizer) {
+        setEvToDel(eventId);
+        setIsConfirmModalVisible(true);
       } else {
-        event.size += 1;
-        event.attendees.push(currentUser.uid);
+        if (event.attendees.includes(currentUser.uid)) {
+          event.size -= 1;
+          event.attendees = event.attendees.filter((e) => e !== currentUser.uid);
+        } else {
+          event.size += 1;
+          event.attendees.push(currentUser.uid);
+        }
+        updateEvent({ ["/events/" + eventId]: event });
       }
-      updateEvent({ ["/events/" + eventId]: event });
-    }
-  };
+    };
 
     const deleteEvent = () => {
-    const newEvents = { ...events };
-    delete newEvents[evToDel];
-    setIsConfirmModalVisible(false);
-    setEvToDel(-1);
-    updateEvent({ "/events": newEvents });
+      const newEvents = { ...events };
+      delete newEvents[evToDel];
+      setIsConfirmModalVisible(false);
+      setEvToDel(-1);
+      updateEvent({ "/events": newEvents });
     };
 
   
-    const userEvents = Object.entries(events).filter((evt) => evt[1].attendees.includes(currentUser.uid))
+    const userEvents = Object.entries(events).filter(([_, evt]) => evt.attendees.includes(currentUser.uid))
    
-    console.log("Inside userpage " + currentUser)
-
     return (
         <>
-        <Container style={{ display: "flex", flexWrap: "wrap" }}>
-          {userEvents
-            .filter(([_, event]) => event.sport === "Basketball" || event.sport === "Soccer")
-            .map(([eventId, event]) => (
-              <EventCard
-                key={eventId}
-                event={event}
-                users={users}
-                eventId={eventId}
-                toggleEvent={toggleEvent}
-                currentUser={currentUser}
-              />
-            ))}
+        <div className='p-5 text-center'>
+            <h1 className='mb-3'>{currentUser.displayName}</h1>
+            <i className='mb-3 text-success'>{currentUser.email}</i>    
+        </div>
+        <h4 className='p-2 text-center'> Welcome back! Keep track of your events here.</h4>
+        <Container>
+          <Row>
+            <Col xs={6}>
+              <h5 className="rounded-4 p-3 text-center" style={{background: '#fae5cf'}}> Events Organized </h5>
+              {userEvents
+                .filter(([_, event]) => event.organizer !== currentUser.uid)
+                .map(([eventId, event]) => (
+                  <EventCard
+                    key={eventId}
+                    event={event}
+                    users={users}
+                    eventId={eventId}
+                    toggleEvent={toggleEvent}
+                    currentUser={currentUser}
+                  />
+                ))}
+            </Col>
+            <Col xs={6}>
+              <h5 className="rounded-4 p-3 text-center" style={{background: '#ffe5e3'}}> Events Joined </h5>
+              {userEvents
+              .filter(([_, event]) => event.organizer === currentUser.uid)
+              .map(([eventId, event]) => (
+                <EventCard
+                  key={eventId}
+                  event={event}
+                  users={users}
+                  eventId={eventId}
+                  toggleEvent={toggleEvent}
+                  currentUser={currentUser}
+                />
+              ))}
+            </Col>
+            </Row>
           <ConfirmModal
             del={deleteEvent}
             showModal={isConfirmModalVisible}
